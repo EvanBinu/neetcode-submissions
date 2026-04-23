d34424861659dc71278cedbadf7844b19e9319f3
Evan Binu Chirayath <143860161+EvanBinu@users.noreply.github.com>
2026-04-23 08:23:44 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> map;
+        for(int i = 0;i<strs.size();i++){
+            string temp = strs[i];
+            sort(temp.begin(),temp.end());
+            map[temp].push_back(strs[i]);
+        }
+        vector<vector<string>> result;
+        for(const auto& [word,words] : map){
+            result.push_back(words);
+        }
+        return result;
+    }
+};
